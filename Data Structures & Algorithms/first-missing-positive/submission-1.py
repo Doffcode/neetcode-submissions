1a6cc6630bdf30e3eca42f977c21698e1702b048
Shivansh Sharma <101421160+Doffcode@users.noreply.github.com>
2026-05-31 06:22:03 +0530

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        # this is O(n logn) solution 
+
+        # nums =  sorted(nums)
+        # i = 1
+        # for n in nums:
+        #     if n == i:
+        #         i+=1
+        # return i
+
+        # O(n) solution with O(n space)
+        
+        seen = set(nums)
+        i = 1
+        while(i <= len(nums)):
+            print (i)
+            if i in seen:
+                i+=1
+            else:
+                print ("the condition is false at i =",i)
+                return i
+        return len(nums)+1
