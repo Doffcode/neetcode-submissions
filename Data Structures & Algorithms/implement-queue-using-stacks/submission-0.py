5d03987b2c7a146529abc6c4669a47a24bb48699
Shivansh Sharma <101421160+Doffcode@users.noreply.github.com>
2026-06-14 22:07:51 +0530

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.py
@@ -0,0 +1,20 @@
+class MyQueue:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        #for i in range (len(self.q)-1):
+
+        return self.q.popleft()
+
+    def peek(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return not self.q
+
+
